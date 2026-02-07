e2cfd23d08718905c3211f50398f7d8dcdddaa9b
Glenvin Rosario <glenrosario84@gmail.com>
2026-02-07 15:46:41 +0530

    Fixed Typescript issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
